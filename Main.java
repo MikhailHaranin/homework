8b999fd52b6ff9c7e86f9351935f33026a808912
mihai <mihail.horanin@gmail.com>
2023-01-30 20:22:03 +0300

    lesson9_hw2

--- Main.java
+++ Main.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Mark ", "23", 2));
+        students.add(new Student("Misha", "24", 3));
+        students.add(new Student("Olya", "25", 7));
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                System.out.println(student.getGroup() + " " + student.getMark());
+            }
+        }
+
+    }
 }
